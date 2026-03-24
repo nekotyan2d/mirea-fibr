9e615b58b36ead9ff92115e016e69899660abe2c
nekotyan2d <dmitryrychazhkov2019@gmail.com>
2026-03-24 09:58:48 +0300

    fix: fix access problem

--- pr11-12/api/app.ts
+++ pr11-12/api/app.ts
@@ -172,8 +172,8 @@ app.use(
 
 // products
 app.post("/api/products", authMiddleware, roleMiddleware(["seller"]), createProduct);
-app.get("/api/products", roleMiddleware(["user"]), getProducts);
-app.get("/api/products/:id", roleMiddleware(["user"]), getProductById);
+app.get("/api/products", getProducts); // доступно всем, даже неавторизованным пользователям
+app.get("/api/products/:id", getProductById); // доступно всем, даже неавторизованным пользователям
 app.patch("/api/products/:id", authMiddleware, roleMiddleware(["seller"]), updateProduct);
 app.delete("/api/products/:id", authMiddleware, roleMiddleware(["seller"]), deleteProduct);
 
